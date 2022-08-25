879245e971d5555d50221505abb9e162de6909fa
noumedem tami florian <noumedemtamiflorian@gmail.com>
2022-08-25 07:00:36 +0100

    update palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,41 +1,64 @@
-function palindrome(str) {
+#  Freecodecamp projects
 
-    let newStr = str.split('').map(elt => {
-        if (/\W|\s|_/.test(elt)) {
-            return ''
-        }
-        return elt.toLowerCase()
-    }).join('')
+Final projects made at the end of the JavaScript Algorithms and Data Structures Certification training! by freecodecamp
 
-    var firstCharacter = function (str) {
-        return str.slice(0, 1);
-    };
+## Palindrome Checker
 
-    var lastCharacter = function (str) {
-        return str.slice(-1);
-    };
+A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
 
-    var middleCharacters = function (str) {
-        return str.slice(1, -1);
-    };
+The palindrome function returns true if the given string is a palindrome.Otherwise, returns false
 
+### palindrome function
 
-    const isPalindrome = (str) => {
-        if (str.length < 2) {
-            return true;
-        }
+- Parameters
+    - `str`: string to check.
+- Return
+        - returns a boolean indicating whether the string sent is a palindrome or not.
+  
 
-        if (firstCharacter(str) == lastCharacter(str)) {
-            return isPalindrome(middleCharacters(str));
-        }
+```json
+result = {
 
-        return false;
-    };
-    return isPalindrome(newStr)
+		"eye":  palindrome("eye"),
+
+		"race car":  palindrome("race car"),
+
+		"_eye":  palindrome("_eye"),
+
+		"not a palindrome":  palindrome("not a palindrome"),
+
+		"A man, a plan, a canal. Panama":  palindrome("A man, a plan, a canal. Panama"),
+
+		"never odd or even":  palindrome("never odd or even"),
+
+		"nope":  palindrome("nope"),
+
+		"almostomla":  palindrome("almostomla"),
+
+		"My age is 0, 0 si ega ym.":  palindrome("My age is 0, 0 si ega ym."),
+
+		"1 eye for of 1 eye.":  palindrome("1 eye for of 1 eye."),
+
+		"0_0 (: /-\ :) 0-0":  palindrome("0_0(: / -\ :) 0 - 0"),
+
+		"five|\_/|four":  palindrome("five|\_/|four")
 }
 
+console.log(result);
 
-console.log(
-    palindrome("eye"),
-    palindrome("race car")
-);
+{
+	  "eye": true,
+	  "race car": true,
+	  "_eye": true,
+	  'not a palindrome': false,
+	  'A man, a plan, a canal. Panama': true,
+	  'never odd or even': true,
+	  "nope": false,
+	  "almostomla": false,
+	  'My age is 0, 0 si ega ym.': true,
+	  '1 eye for of 1 eye.': false,
+	  '0_0 (: /- :) 0-0': true,
+	  'five|_/|four': false
+}
+
+```
